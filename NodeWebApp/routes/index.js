651b51b9f277595d69edb4e4f5fcfd753606656b
Arun Malik <arunma@microsoft.com>
2016-02-20 01:05:28 -0800

    utc time updated and some minor changes

--- NodeWebApp/routes/index.js
+++ NodeWebApp/routes/index.js
@@ -19,27 +19,27 @@ router.get('/', function(req, res, next) {
      
     var todaysDate = new Date();
     var yesterdaysDate = new Date(); 
-    todaysDate.setDate(yesterdaysDate.getDate()+1); 
+    todaysDate.setDate(todaysDate.getDate()  + 1); 
     yesterdaysDate.setHours(0, 0, 0, 0);
     todaysDate.setHours(0, 0, 0, 0);
     
     var queryParams = req.query;
-      console.log(queryParams);
+     
      if(typeof queryParams !== 'undefined'  &&  Object.keys(queryParams).length > 0 && queryParams ){
          
          yesterdaysDate= new Date(queryParams.historyStartDate);
-         yesterdaysDate.setDate(yesterdaysDate.getDate()+1);
-         yesterdaysDate.setHours(0, 0, 0, 0);
+        // yesterdaysDate.setDate(yesterdaysDate.getDate()+1);
+       //  yesterdaysDate.setHours(0, 0, 0, 0);
          todaysDate= new Date(queryParams.historyEndDate);
-         todaysDate.setDate(yesterdaysDate.getDate()+1);
-         todaysDate.setHours(0, 0, 0, 0);
+        // todaysDate.setDate(yesterdaysDate.getDate()+1);
+        // todaysDate.setHours(0, 0, 0, 0);
      } 
      
-    console.log(yesterdaysDate);
-    console.log(todaysDate);
- 
+     console.log('startDate: '+ yesterdaysDate);
+     console.log('endDate: '+ todaysDate);
+     
      var tasks = new Task(azure.createTableService(accountName, accountKey), tableName, partitionKey);
-     var query = new azure.TableQuery().where('PartitionKey eq ? and dateTime >= ?  and dateTime < ?', 'comcast',yesterdaysDate, todaysDate);
+     var query = new azure.TableQuery().where('PartitionKey eq ? and dateTime > ?  and dateTime <= ?', 'comcast',yesterdaysDate, todaysDate);
     //var tasksObject = req.app.get('tasks');
      tasks.find(query, function(error, results) { 
      // console.log(results);
@@ -53,6 +53,9 @@ router.get('/', function(req, res, next) {
         results.sort(function(a,b){ 
             return b.dateTime._ - a.dateTime._;
         });
+        yesterdaysDate.setDate(yesterdaysDate.getDate()+1);
+        todaysDate.setDate(yesterdaysDate.getDate()+1);
+        
        res.render('index', { tabular: results , charts : charts, historyEndDate : todaysDate ,historyStartDate: yesterdaysDate });
     });
  
